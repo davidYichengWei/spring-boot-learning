ce7171aa302782f8f432feb4bb71442a0cd43018
davidYichengWei <a1093888167@hotmail.com>
2023-01-04 01:08:45 -0500

    Use Spring Bean to return JSON

--- restful-springboot/src/main/java/com/rest/springboot/restfulspringboot/helloworld/HelloWorldController.java
+++ restful-springboot/src/main/java/com/rest/springboot/restfulspringboot/helloworld/HelloWorldController.java
@@ -3,11 +3,23 @@ package com.rest.springboot.restfulspringboot.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// DispatcherServlet at path / gets auto-configured by Auto Configuration,
+// and redirects the request to the controller with the request mapping path
+
 @RestController
+// RestController has the annotation @ResponseBody, which returns the 
+// return values as response body
 public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	// When a bean is returned, the default message conversion JacksonHttpMessageConverters
+	// configured by Auto Configuration converts it to JSON format
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 }
