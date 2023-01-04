d4b297cc86ae34b6c0e895d1a5f892c219b70a76
davidYichengWei <a1093888167@hotmail.com>
2023-01-04 17:21:05 -0500

    Add PathVariable

--- restful-springboot/src/main/java/com/rest/springboot/restfulspringboot/helloworld/HelloWorldController.java
+++ restful-springboot/src/main/java/com/rest/springboot/restfulspringboot/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.rest.springboot.restfulspringboot.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 // DispatcherServlet at path / gets auto-configured by Auto Configuration,
 // and redirects the request to the controller with the request mapping path
@@ -21,5 +22,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	// Use PathVariable to customize the API endpoint path
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
